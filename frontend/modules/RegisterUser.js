7e2d4d680f5c69b1f48d721249eefcc9f4bbe7fe
sidemar <sidemarschimmelpfennig@gmail.com>
2023-12-26 00:44:58 -0300

    new

---
+++ frontend/modules/RegisterUser.js
@@ -0,0 +1,31 @@
+
+export default class RegisterUser {
+    constructor(formClass){
+        this.form = document.querySelector(formClass)
+    }
+    init(){
+        this.events()
+    }
+    events(){
+        if(!this.form) return
+        this.form.addEventListener('submit', e => {
+            e.preventDefault()
+            this.validate(e)
+        })
+    }
+    validate(e){
+        const element = e.target
+        const emailInput = element.querySelector('input[name="email"]')
+        const passwordInput = element.querySelector('input[name="password"]')
+        let error = false
+        if (!validator.isEmail(emailInput.value)){
+            alert('Email inválido')
+            error = true
+        }
+        if(passwordInput.value.length < 3 || passwordInput.value.length > 50 ){
+            alert('Senha precisa ter entre 3 e 50  caracteres')
+            error = true
+        }
+        if(!error)element.submit()
+    }
+}
